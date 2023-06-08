4c46fef7fa81a6aa578483416ad1f100ebbe298f
hunterdyar <bloop@hdyar.com>
2023-06-08 12:19:55 -0400

    Round Manager Overlay

---
+++ Assets/Scripts/Turns/Editor/RoundManagerOverlay.cs
@@ -0,0 +1,26 @@
+﻿using Tactics.Turns;
+using UnityEditor;
+using UnityEditor.Overlays;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace Turns.Editor
+{
+	[Overlay(typeof(SceneView), "Round Manager View", true)]
+	public class RoundManagerOverlay : Overlay
+	{
+		public override VisualElement CreatePanelContent()
+		{
+			var root = new VisualElement(){name = "Round Manager Overlay"};
+			root.Add(new Label(){text = "Round Manager"});
+
+			var startRound = new Button(() => { GameObject.FindObjectOfType<RoundManager>()?.StartRound(); })
+			{
+				text = "Start Round"
+			};
+			root.Add(startRound);
+
+			return root;
+		}
+	}
+}
